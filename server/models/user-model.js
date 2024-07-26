27cc88b07a01a48adb006babd0e6f287f75bdcea
ddube004 <deepak.dubey456@gmail.com>
2024-07-27 01:19:08 +0530

    main

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -65,6 +65,10 @@ userSchema.methods.generateToken = async function () {
   }
 };
 
+
+
+
+
 // comparePassword
 userSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
